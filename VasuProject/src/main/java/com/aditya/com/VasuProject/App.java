d88d9a79fe22cf1eb358279fc2c0d797c10cc8a2
Srinivas <adityasrinivasb@gmail.com>
2017-03-19 13:04:36 +0530

    First Commit

---
+++ VasuProject/src/main/java/com/aditya/com/VasuProject/App.java
@@ -0,0 +1,13 @@
+package com.aditya.com.VasuProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
